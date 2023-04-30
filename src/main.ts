36835621b9573853c425143ac08de780dbe4644a
Cameron Tacklind <cameron@tacklind.com>
2023-04-30 16:58:31 -0700

    Reading data from oven!

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+import { SerialPort } from 'serialport';
+import { ReadlineParser } from '@serialport/parser-readline';
+
+const port = new SerialPort({ path: '/dev/serial0', baudRate: 115200 });
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+parser.on('data', console.log);
+
 console.log('Dashboard main');
