2f75c7400089d3f4e09a77d305210a368168a448
Cameron Tacklind <cameron@tacklind.com>
2024-05-12 21:41:33 -0700

    Make sleep cancellable

--- src/util/sleep.ts
+++ src/util/sleep.ts
@@ -1,3 +1,18 @@
-export function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+type CancelableTimeout = Promise<void> & { cancel: () => void };
+
+export function sleep(ms: number): CancelableTimeout {
+  let cancel: () => void;
+
+  const ret = new Promise<void>(resolve => {
+    const to = setTimeout(resolve, ms);
+    cancel = () => {
+      clearTimeout(to);
+      resolve();
+    };
+  }) as CancelableTimeout;
+
+  if (!cancel!) throw new Error('cancel is not defined?');
+  ret.cancel = cancel;
+
+  return ret;
 }
